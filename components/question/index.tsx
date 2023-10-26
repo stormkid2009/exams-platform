f74dda508b9a5240d9ac7fdfce63fc94f1840e1e
Anwar Ahmed <nileking2007@gmail.com>
2023-10-26 23:06:08 +0200

    working on question input form

--- components/question/index.tsx
+++ components/question/index.tsx
@@ -1,10 +1,10 @@
 // container of the question
-
+//lets think about the question component 
+// it will be used to display the question only
+// or we can use it to input the question too
 import Header from "./header"
 import Content from "./content"
 import Answers from "./answers"
-import NextBtn from "../buttons/nextBtn"
-import PreviousBtn from "../buttons/previousBtn"
 //we will render one of the buttons or both conditionally
 //the type of question determines this.
 
