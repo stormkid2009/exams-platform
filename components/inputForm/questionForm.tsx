6fc58283e57b34e9c57667f0cf3e760a8cb69fab
Anwar Ahmed <nileking2007@gmail.com>
2023-10-26 23:50:27 +0200

    working with question form input

--- components/inputForm/questionForm.tsx
+++ components/inputForm/questionForm.tsx
@@ -3,26 +3,52 @@ import React from "react";
 function QuestionForm() {
   return (
     <div>
-      <form action="api/dashboard/question/new" method="POST">
+      <h2 className="text-center">after fill the from please click submit</h2>
+      <form action="api/dashboard/question/new" method="POST" className="flex flex-col items-center">
+        <div>
+
         <label htmlFor="document">
           <input type="text" id="document-question" name = "documentQuestion"/>
         </label>
+        </div>
+        <div>
+
         <label htmlFor="header">
           <input type="text" id="question-header" name = "questionHeader" required/>
         </label>
+        </div>
+        <div>
+
         <label htmlFor="firstOption">
+          A)
+        </label>
           <input type="text" id="first-option" name="firstOption" required/>
-        </label>
+        </div>
+        <div>
+
         <label htmlFor="secondOption">
+          B)
+        </label>
           <input type="text" id="second-option" name="secondOption"  required/>
-        </label>
+        </div>
+        <div>
+
         <label htmlFor="thirdOption">
+          C)
+        </label>
           <input type="text" id="third-option" name="thirdOption" required/>
-        </label>
+        </div>
+        <div>
+
         <label htmlFor="fourthOption">
-          <input type="text" id="fourth-option" name="fourthOption" required/>
+          D)
         </label>
+          <input type="text" id="fourth-option" name="fourthOption" required/>
+        </div>
+        <div>
+
         <button type="submit">Submit</button> 
+        </div>
       </form>
     </div>
   );
