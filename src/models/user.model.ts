02c28a8507423af35725e8e6381d10ee7f72970d
Anwar Ahmed <nileking2007@gmail.com>
2024-12-10 15:10:43 +0200

    chore: update to fit user auth

---
+++ src/models/user.model.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import { IUserModel } from "src/types";
+
+
+const userSchema: Schema<IUserModel> = new Schema({
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
