11c62019134578c0765bbaeaeefc4d521c44eb02
Anwar Ahmed <nileking2007@gmail.com>
2024-12-12 00:17:07 +0200

    fix: update mongodb string

--- src/lib/mongooseClient.ts
+++ src/lib/mongooseClient.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI:string | undefined = process.env.MONGODB_LOCAL_URI;
+const MONGODB_URI:string | undefined = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error(
