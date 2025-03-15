a664974135f732c53491b9894dd1818dd455dfa2
Anwar Ahmed <nileking2007@gmail.com>
2025-03-15 23:19:47 +0200

    feat: add docs to navigate button

--- src/components/buttons/navigate/index.tsx
+++ src/components/buttons/navigate/index.tsx
@@ -1,3 +1,22 @@
+/**
+ * NavigateBtn Component
+ *
+ * A button component which navigates to a specified route when clicked.
+ * It leverages Next.js's useRouter hook for client-side navigation.
+ *
+ * @component
+ * @param {object} props - Component properties.
+ * @param {string} props.path - The target route to navigate to.
+ * @param {React.ReactNode} props.children - The content inside the button.
+ * @param {string} [props.color="blue"] - The color name used to style the button (tailwindcss classes).
+ *
+ * @example
+ * // Navigate to the "about" page
+ * <NavigateBtn path="/about" color="green">
+ *   Go to About
+ * </NavigateBtn>
+ */
+
 import React from "react";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
@@ -8,10 +27,10 @@ interface NavigateBtnProps {
   color?: string;
 }
 
-export const NavigateBtn = ({ 
-  path, 
-  children, 
-  color = "blue" 
+export const NavigateBtn = ({
+  path,
+  children,
+  color = "blue",
 }: NavigateBtnProps) => {
   const router = useRouter();
 
