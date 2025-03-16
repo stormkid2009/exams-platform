407279c5e4f0107bd5eb0ff9d483f57a611448b3
Anwar Ahmed <nileking2007@gmail.com>
2025-03-17 00:14:59 +0200

    feat: add docs to option input

--- src/components/inputs/option-input.tsx
+++ src/components/inputs/option-input.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import { UseFormRegister, FieldValues, Path } from "react-hook-form";
 import BaseInput from "./base-input"; // Import the BaseInput component
+/**
+ * OptionInput Component
+ * 
+ * This component renders an input field for entering an option value. 
+ * It integrates with react-hook-form for form handling.
+ * 
+ * Props:
+ * - register (function): The register function from react-hook-form for input registration.
+ * - name (string): The name of the input field.
+ * - label (string): The label for the input field.
+ * - placeholder (string, optional): An optional placeholder for the input.
+ * - errorMessage (string, optional): An optional error message to display below the input.
+ * 
+ * The component uses the BaseInput component to render the input field with a default validation rule 
+ * to ensure the field is required.
+ */
 
 interface OptionInputProps<T extends FieldValues> {
   register: UseFormRegister<T>;
