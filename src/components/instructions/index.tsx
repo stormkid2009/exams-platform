a8f650094a9e4ec5df0d581743166ac1238b9371
Anwar Ahmed <nileking2007@gmail.com>
2025-03-17 00:16:28 +0200

    feat: add docs to instructions component

--- src/components/instructions/index.tsx
+++ src/components/instructions/index.tsx
@@ -1,7 +1,16 @@
 // display the instructions which the user will follow
 import React from "react"
 
-
+/**
+ * Instructions Component
+ * 
+ * This component displays instructions for the user regarding the test process. 
+ * It provides guidance on what the user needs to do before starting the test.
+ * 
+ * The component includes:
+ * - A message instructing the user to complete a task before starting the test.
+ * - A prompt to click a button to begin the test when ready.
+ */
 function Instructions() {
   return (
     <div className="text-center font-mono text-xl">
