274ce7ab70dc6e71c1d57d73ff04e72b103930c9
Anwar Ahmed <nileking2007@gmail.com>
2025-03-17 00:08:10 +0200

    feat: add docs to header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,6 +4,20 @@ import DateDisplay from "./date-display";
 import Logo from "./logo";
 import PlatformTitle from "./platform-title";
 
+/**
+ * Header Component
+ * 
+ * This component renders the header section of the application, 
+ * which includes the platform logo, title, and the current date display.
+ * 
+ * The header is styled to be responsive and visually appealing, 
+ * with a background color and text color that enhances readability.
+ * 
+ * The component includes:
+ * - Logo: Displays the platform's logo.
+ * - PlatformTitle: Displays the title "Tests Generator".
+ * - DateDisplay: Shows the current date, updating every 30 seconds.
+ */
 function Header() {
   return (
     <header className="w-full rounded-t-lg flex items-center justify-between bg-slate-700 text-white p-4 md:p-6">
