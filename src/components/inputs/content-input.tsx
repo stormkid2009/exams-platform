b454c830765dc73f123334ec78b9f42a8fcef372
Anwar Ahmed <nileking2007@gmail.com>
2025-03-17 00:12:48 +0200

    feat: add docs to content input

--- src/components/inputs/content-input.tsx
+++ src/components/inputs/content-input.tsx
@@ -2,6 +2,23 @@ import React from "react";
 import { UseFormRegister, FieldValues, Path } from "react-hook-form";
 import BaseInput from "./base-input";
 
+/**
+ * ContentInput Component
+ * 
+ * This component renders a textarea input for entering content. 
+ * It integrates with react-hook-form for form handling.
+ * 
+ * Props:
+ * - register (function): The register function from react-hook-form for input registration.
+ * - name (string): The name of the input field.
+ * - label (string): The label for the input field.
+ * - placeholder (string, optional): An optional placeholder for the input.
+ * - errorMessage (string, optional): An optional error message to display below the input.
+ * - rows (number, optional): The number of rows for the textarea (default is 3).
+ * 
+ * The component uses the BaseInput component to render the textarea and 
+ * applies default validation rules to ensure the field is required.
+ */
 interface ContentInputProps<T extends FieldValues> {
   register: UseFormRegister<T>;
   name: Path<T>; // Use Path<T> instead of string
