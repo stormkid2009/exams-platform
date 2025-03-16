9e05b9f98cda20ef66153320d4b3f4c2b6f71945
Anwar Ahmed <nileking2007@gmail.com>
2025-03-17 00:10:09 +0200

    feat: add docs to answer input

--- src/components/inputs/answer-input.tsx
+++ src/components/inputs/answer-input.tsx
@@ -7,6 +7,25 @@ import {
 } from "react-hook-form";
 import BaseInput from "./base-input"; // Import the BaseInput component
 
+/**
+ * AnswerInput Component
+ * 
+ * This component renders a select input for choosing an answer option. 
+ * It allows users to select from a predefined set of options or custom options.
+ * 
+ * Props:
+ * - register (function): The register function from react-hook-form for input registration.
+ * - name (string): The name of the input field.
+ * - errorMessage (string, optional): An optional error message to display.
+ * - maxOptions (4 | 5): The maximum number of options allowed (4 or 5).
+ * - label (string, optional): An optional label for the input.
+ * - placeholder (string, optional): An optional placeholder for the input.
+ * - options (array, optional): An optional array of custom options to display.
+ * - registerOptions (object, optional): Custom validation rules for the input.
+ * 
+ * The component validates the maxOptions prop to ensure it is either 4 or 5, 
+ * and defaults to generating options labeled 'A', 'B', 'C', etc.
+ */
 interface AnswerInputProps<T extends FieldValues> {
   register: UseFormRegister<T>;
   name: Path<T>; // The name of the input field
