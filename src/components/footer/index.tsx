b979aad2d798a114e4e27fe041c57a0ef08c0cf8
Anwar Ahmed <nileking2007@gmail.com>
2025-03-16 23:51:58 +0200

    feat: add docs to footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import CopyRights from "./copy-rights";
-
+/**
+ * Footer Component
+ * 
+ * This component renders the footer section of the application, 
+ * which includes copyright information displayed by the CopyRights component.
+ */
 function Footer() {
   return (
     <footer className="w-full rounded-b-lg bg-slate-700 text-white py-4">
