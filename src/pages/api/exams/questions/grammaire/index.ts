2c3e581c826a89d55ab48b1ed8d521bb196d269e
Anwar Ahmed <nileking2007@gmail.com>
2025-03-28 14:49:00 +0200

    feat: - add functionality to api for passage random question - make msg as const in both passage and gramamire questions

--- src/pages/api/exams/questions/grammaire/index.ts
+++ src/pages/api/exams/questions/grammaire/index.ts
@@ -9,25 +9,24 @@ const msg:Messages = {
   failure:"failed to retrieve random grammaire question",
   wrongMethod:"the method is not valid",
   invalidData:"the data is not valid",
-} ;
+} as const;
+
+
 const handler = async(
   req:NextApiRequest,
   res:NextApiResponse
 ):Promise<void> =>{
-  //const path = "api/exams/questions/grammaire";
+
   const method = req.method || "UNKNOWN";
 
   if(method !== "GET"){
     return res.status(400).json(msg.wrongMethod);
-
   }
   try{
 
   const result = await GrammaireService.getRandomQuestion({});
 
   if(!result.success){
-    //show msg
-    //return status code number
     console.log(msg.failure);
     return res.status(result.error?.code || 500).json({error:result.error});
   }
---
+++ src/pages/api/exams/questions/passage/index.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest,NextApiResponse } from "next";
+import { PassageService } from "src/services/passage.service";
+import { Messages } from "src/types/common";
+
+
+
+
+const msg:Messages = {
+  success:"successfully retrieve random grammaire question  ",
+  failure:"failed to retrieve random grammaire question",
+  wrongMethod:"the method is not valid",
+  invalidData:"the data is not valid",
+} as const;
+const handler=async(
+  req:NextApiRequest,
+  res:NextApiResponse
+): Promise<void> =>{
+
+  const method = req.method || "UNKNOWN";
+  if(method !== "GET"){
+    return res.status(400).json({error:msg.wrongMethod});
+  }
+  
+  try {   
+ 
+    const result = await PassageService.getRandomQuestion({});
+
+  if(!result.success){
+      console.log(msg.failure);
+      return res.status(result.error?.code || 500).json({error:result.error});
+    }
+  res.status(200).json(result.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error:msg.invalidData})
+  }
+}
+
+export default handler;
